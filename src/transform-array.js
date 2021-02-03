b3621484c447b5c3d6883db7a2555fb35f19be67
Nazar Pidhirnyi <nazar.pidhirnyi@gmail.com>
2021-02-03 13:25:51 +0200

    refactor: delete unused code

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,3 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = function transform(arr) {
   if (Array.isArray(arr)) {
     let newArr = []
@@ -22,34 +20,3 @@ module.exports = function transform(arr) {
     return newArr
   } else throw Error
 };
-
-function transform(arr) {
-  if (Array.isArray(arr)) {
-    let newArr = []
-    let isDiscard = false
-    for (let i = 0; i < arr.length; i++) {
-      if (!isDiscard) {
-        if (arr[i] == '--double-next') {
-          newArr.push(arr[i + 1])
-        } else if (arr[i] == '--double-prev') {
-          newArr.push(arr[i - 1])
-        } else if (arr[i] == '--discard-prev') {
-          newArr.pop()
-        } else if (arr[i] == '--discard-next') {
-          isDiscard = true
-        } else if (arr[i]) {
-          newArr.push(arr[i])
-        }
-      } else isDiscard = false
-    }
-    return newArr
-  } else throw Error
-};
-
-
-console.log(transform([undefined, 1, 2, 3, '--discard-next', 4, 5, undefined]));
-console.log(transform([1, 2, 3, '--discard-prev', 4, 5]));
-
-// `transform([1, 2, 3, '--double-next', 4, 5])` `[1, 2, 3, 4, 4, 5]`
-
-  // `transform([1, 2, 3, '--discard-prev', 4, 5])`  `[1, 2, 4, 5]`
