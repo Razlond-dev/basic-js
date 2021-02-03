2ee1ae8341132a43db3caf34efd635bda4e53e4a
Nazar Pidhirnyi <nazar.pidhirnyi@gmail.com>
2021-02-03 17:44:24 +0200

    feat: solve simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,61 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof value == 'function') {
+      value = value.toString()
+    }
+    if (this.chain.length > 0) {
+      this.chain.push(`~~( ${value} )`)
+      return chainMaker
+    } else {
+      this.chain.push(`( ${value} )`)
+      return chainMaker
+    }
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position > 0 && position < this.chain.length) {
+      this.chain.splice(position - 1, 1)
+      if (position == 1) {
+        this.handleTilda()
+      }
+
+      return chainMaker
+    } else {
+      this.chain = []
+      throw Error
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse()
+    this.handleTilda()
+    this.handleTildaInTheEnd()
+    return chainMaker
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let stringToReturn = this.chain.join('')
+    this.chain = []
+    return stringToReturn
+  },
+  handleTilda() {
+    if (this.chain.length > 1) {
+      if (this.chain[0].slice(0, 2) == '~~') {
+        this.chain[0] = this.chain[0].replace('~~(', '(')
+      }
+
+      if (this.chain[1].slice(0, 2) !== '~~') {
+        this.chain[1] = this.chain[1].replace('(', '~~(')
+      }
+    }
+  },
+  handleTildaInTheEnd() {
+    if (this.chain.length > 1) {
+      if (this.chain[this.chain.length - 1].slice(0, 2) !== '~~') {
+        this.chain[this.chain.length - 1] = this.chain[this.chain.length - 1].replace('(', '~~(')
+      }
+    }
   }
 };
 
